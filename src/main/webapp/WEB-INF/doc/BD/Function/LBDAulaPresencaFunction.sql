be83b7e68518c1c4f688f445a42656af46c1a780
Matheus França <matheussss6613@gmail.com>
2024-05-13 07:11:06 -0300

    fix function sql

--- src/main/webapp/WEB-INF/doc/BD/Function/LBDAulaPresencaFunction.sql
+++ src/main/webapp/WEB-INF/doc/BD/Function/LBDAulaPresencaFunction.sql
@@ -12,12 +12,12 @@ RETURNS @table TABLE
 )
 BEGIN
 	INSERT INTO @table
-		SELECT COUNT(p.id), p.id_matricula, p.id_conteudo, m.ra_aluno, a2.nome, p.status FROM presenca p, aula a, matricula m, aluno a2
+		SELECT COUNT(p.id), p.id_matricula, p.id_conteudo, m.ra_aluno, a2.nome, MAX(CASE p.status WHEN 1 THEN 0 ELSE 0 END) AS status  FROM presenca p, aula a, matricula m, aluno a2
 		WHERE p.id_matricula = a.id_matricula 
 			AND p.id_conteudo = a.id_conteudo 
 			AND a.id_matricula = m.id 
 			AND m.ra_aluno = a2.ra
 			AND p.id_conteudo = @id
-		GROUP BY p.id_matricula, p.id_conteudo, m.ra_aluno, a2.nome, p.status
+		GROUP BY p.id_matricula, p.id_conteudo, m.ra_aluno, a2.nome
 	RETURN
 END
