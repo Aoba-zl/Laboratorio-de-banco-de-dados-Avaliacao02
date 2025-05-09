d0b75a010cafa4ca3aab35b91053b8c4ed3d592c
david <david@DESKTOP-SQL0MNR>
2025-05-09 12:09:14 -0300

    add test get aluno

--- src/test/java/com/fatec/LBDAvaliacao02/LbdAvaliacao02ApplicationTests.java
+++ src/test/java/com/fatec/LBDAvaliacao02/LbdAvaliacao02ApplicationTests.java
@@ -1,13 +1,20 @@
 package com.fatec.LBDAvaliacao02;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.ui.ModelMap;
+
+import com.fatec.LBDAvaliacao02.servlet.AlunoServlet;
 
 @SpringBootTest
 class LbdAvaliacao02ApplicationTests {
-
+	@Autowired
+	private AlunoServlet AlunoServlet;
 	@Test
 	void contextLoads() {
+		ModelMap modelMap = new ModelMap();
+		AlunoServlet.alunoGet(modelMap);
 	}
 
 }
